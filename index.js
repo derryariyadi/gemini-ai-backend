a3c2767b892036e8f921eb58c118ef3e15c251ac
thereddas <thereddas@gmail.com>
2025-08-02 17:47:44 +0700

    adding public starter

--- index.js
+++ index.js
@@ -3,6 +3,9 @@ import express from "express";
 import multer from "multer";
 import fs from "fs/promises";
 import { GoogleGenAI } from "@google/genai";
+import cors from "cors";
+import path from "path";
+import { fileURLToPath } from "url";
 
 const extractGeneratedText = (data) => {
   try {
@@ -19,10 +22,33 @@ const extractGeneratedText = (data) => {
 };
 
 const app = express();
-app.use(express.json());
+
 const upload = multer();
 
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 const DEFAULT_GEMINI_MODEL = "gemini-2.5-flash";
+
+const modelMapper = {
+  flash: "gemini-2.5-flash",
+  "flash-light": "gemini-2.5-flash-lite",
+  pro: "gemini-2.5-pro",
+};
+
+const determineGeminiModel = (key) => {
+  return modelMapper[key] ?? DEFAULT_GEMINI_MODEL;
+};
+
+// memanggil middleware untuk bisa handle CORS
+// app.use(cors);
+
+// memanggil middleware untuk bisa terima header
+// dengan Content-Type: application/json
+app.use(express.json());
+
+// panggil middleware untuk serve static file
+// app.use(express.static(path.join(__dirname, "public")));
 
 const ai = new GoogleGenAI({
   apiKey: process.env.GOOGLE_AI_STUDIO_KEY,
@@ -85,6 +111,49 @@ app.post(
     }
   }
 );
+
+app.post("/chat", async (req, res) => {
+  try {
+    if (!req.body) {
+      res.status(400).json({ message: "Body is required" });
+    }
+
+    const { message } = req.body;
+
+    if (!message) {
+      res.status(400).json({ message: "Message is required" });
+      return;
+    }
+
+    if (!Array.isArray(message)) {
+      res.status(400).json({ message: "Message must be an array" });
+      return;
+    }
+
+    const payload = message.map((msg) => {
+      return {
+        role: msg.role,
+        parts: [
+          {
+            text: msg.content,
+          },
+        ],
+      };
+    });
+
+    const aiResponse = await ai.models.generateContent({
+      model: determineGeminiModel("flash"),
+      contents: payload,
+      config: {
+        systemInstruction: "Anda adalah chatter terhandal",
+      },
+    });
+
+    res.json({ result: extractGeneratedText(aiResponse) });
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
+});
 
 app.listen(process.env.DEFAULT_PORT, () => {
   console.log(`Server listening on port ${process.env.DEFAULT_PORT}`);
